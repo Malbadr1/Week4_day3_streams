0ca55741690830711c5b889c78a1b38bce11588f
Malbadr1 <Albadri@gmx.net>
2020-11-18 16:41:13 +0100

    ..

--- src/alphabetical_list.java
+++ src/alphabetical_list.java
@@ -1,90 +1,49 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Scanner;
 
-public class alphabetical_list{
+public class alphabetical_list {
 
 
+    public static void main(String args[])  {
 
-    public static void main(String args[]) throws IOException
-    {
+       sortWords("g://data");
 
-        try
-        {
-            FileReader fr = new FileReader("g://data");
-            BufferedReader br = new BufferedReader(fr);
-            String str;
-
-            while((str = br.readLine()) != null)
-                countwords(str);
-            fr.close();
-        }
-        catch(FileNotFoundException e)
-        {
-            System.out.println("File Not Found");
-        }
-        catch(IOException e)
-        {
-            System.out.println("Exception : " + e);
-        }
     }
 
-    static void countwords(String st)
-    {
-        int len,i,totwords=1,j=-1,k=0,freqarr[],temp;
-        int cnt=0,flag=0;
-        len = st.length();
-        char ch[];
-        String starr[],st1[];
+    public static ArrayList sortWords(String nameOfFile){
+        ArrayList<String> sort=new ArrayList<>();
+        try {
+            File file=new File("g://data");
+            Scanner scanner=new Scanner(file);
+            while (scanner.hasNext()){
+                String output= scanner.next();
+                sort.add(output);
+            }
+            scanner.close();
 
-        for(i=0;i<len-1;i++)
-        {
-            if(st.charAt(i) != ' ' && st.charAt(i+1) == ' ')
-                totwords++;
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
 
-        starr = new String[totwords+1];
-        freqarr = new int[totwords];
-        st1 = new String[totwords];
+        System.out.println("before sort : ");
 
-        for(i=0;i<len-1;i++)
-        {
-            if(st.charAt(i) != ' ' && st.charAt(i+1) == ' ')
-            {
-                ch = new char[(i+1) - j];
-                st.getChars(j+1,i+1,ch,0);
-                starr[k++] = new String(ch);
-                j = i+1;
-            }
+        for (String i:sort) {
+            System.out.println(i + " ");
         }
 
-        ch = new char[(i+1) - j];
-        st.getChars(j+1,i+1,ch,0);
-        starr[k] = new String(ch);
-        k=0;
+        Collections.sort(sort);
+            System.out.println("after sort");
 
-        for(i=0;i<totwords;i++)
-        {
-            temp = 1;
-            flag = 0;
-            for(j=i+1;j<=totwords;j++)
-            {
-                if(starr[i].equals(starr[j]))
-                    temp++;
-            }
-            freqarr[k] = temp;
+            for (String i:sort) {
+                System.out.println(i+" ");
 
-            for(cnt=0;cnt<k;cnt++)
-            {
-                if(starr[i].equals(st1[cnt]))
-                    flag = 1;
-            }
-            if(flag == 0)
-                st1[k++] = starr[i];
         }
-
-        for(i=0;i<k;i++)
-            System.out.println(st1[i] + "has count : " + freqarr[i]);
-       
+            return sort;
     }
-
 
 }
+
